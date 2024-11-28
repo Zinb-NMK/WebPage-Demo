05ae6734e78f3a9485e68345910449d7a0186ac1
N.ManojKumar <nagarammanojkumar333@gmail.com>
2024-11-28 12:01:48 +0530

    New content added

--- Cource/Loops.py
+++ Cource/Loops.py
@@ -1,7 +1,16 @@
 def main():
-    print_row(4)
+    print_square(4)
 
-def print_row(weidth):
-    print("?" * weidth)
+def print_square(size):
+
+    #for each r ow in square
+    for i in range(size):
+
+        #for each brick in row
+        for j in  range(size):
+
+            #print brick
+            print("#", end="")
+        print()
 
 main()
